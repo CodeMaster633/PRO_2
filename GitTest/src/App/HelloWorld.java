9acfd062fd4d487381b32b865d30f3e9c165da3f
CodeMaster633 <126244327+CodeMaster633@users.noreply.github.com>
2023-08-24 09:34:50 +0200

    Initial commit 2

--- GitTest/src/App/HelloWorld.java
+++ GitTest/src/App/HelloWorld.java
@@ -2,6 +2,8 @@ package App;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
         System.out.println("Hello World");
+        System.out.println("Hello World, again");
     }
 }
